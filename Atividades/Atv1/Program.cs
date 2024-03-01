560a93fc296ca1754c3ba93c6a44455ade005756
Sara <saragibmaier@gmail.com>
2024-03-01 20:30:20 -0300

    Atividade1

--- Atividades/Atv1/Program.cs
+++ Atividades/Atv1/Program.cs
@@ -1,20 +1,72 @@
 ﻿using System;
 
-string nome = string.Empty;
-Console.WriteLine( " Qual é o seu nome?");
-nome = Console.ReadLine();
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = string.Empty;
+        Console.WriteLine("Nome:");
+        nome = Console.ReadLine();
 
-string nasc = string.Empty;
-Console.WriteLine( " Qual sua data de nascimento");
-nasc = Console.ReadLine();
+        string email = string.Empty;
+        Console.WriteLine("E-mail:");
+        email = Console.ReadLine();
 
-string endereco = string.Empty;
-Console.WriteLine( "Qual seu endereço?");
-endereco = Console.ReadLine();
+        string nasc = string.Empty;
+        Console.WriteLine("Data de nascimento:");
+        nasc = Console.ReadLine();
 
-string email = string.Empty;
-Console.WriteLine( " Qual seu e-mail?");
-email = Console.ReadLine();
+        string genero = string.Empty;
+        Console.WriteLine("Sexo ou gênero:");
+        genero = Console.ReadLine();
 
-string resultado = $" Nome: {nome} \n Nasc: {nasc} ";
-Console.WriteLine(resultado);
+        string cep = string.Empty;
+        Console.WriteLine("CEP:");
+        cep = Console.ReadLine();
+
+        string rua = string.Empty;
+        Console.WriteLine("Rua:");
+        rua = Console.ReadLine();
+
+        string numero = string.Empty;
+        Console.WriteLine("Número:");
+        numero = Console.ReadLine();
+
+        string bairro = string.Empty;
+        Console.WriteLine("Bairro:");
+        bairro = Console.ReadLine();
+
+        string cidade = string.Empty;
+        Console.WriteLine("Cidade:");
+        cidade = Console.ReadLine();
+
+        string uf = string.Empty;
+        Console.WriteLine("UF:");
+        uf = Console.ReadLine();
+
+        string pais = string.Empty;
+        Console.WriteLine("País:");
+        pais = Console.ReadLine();
+
+        Console.WriteLine("\n+-------------------------------------+");
+        Console.WriteLine("|           Informações Finais         |");
+        Console.WriteLine("+-------------------------------------+");
+
+        Console.WriteLine("+---------------------+----------------+");
+        Console.WriteLine("|      Informação     |      Dados     |");
+        Console.WriteLine("+---------------------+----------------+");
+        Console.WriteLine($"| Nome                | {nome,-15} |");
+        Console.WriteLine($"| E-mail              | {email,-15} |");
+        Console.WriteLine($"| Data de nascimento  | {nasc,-15} |");
+        Console.WriteLine($"| Sexo ou gênero      | {genero,-15} |");
+        Console.WriteLine($"| CEP                 | {cep,-15} |");
+        Console.WriteLine($"| Rua                 | {rua,-15} |");
+        Console.WriteLine($"| Número              | {numero,-15} |");
+        Console.WriteLine($"| Bairro              | {bairro,-15} |");
+        Console.WriteLine($"| Cidade              | {cidade,-15} |");
+        Console.WriteLine($"| UF                  | {uf,-15} |");
+        Console.WriteLine($"| País                | {pais,-15} |");
+        Console.WriteLine("+---------------------+----------------+");
+    }
+    
+}
