c807d8c528bb8d3c2b235b7885dd51b85e1bd95c
Sara <saragibmaier@gmail.com>
2024-03-16 09:42:11 -0300

    atv3.1

---
+++ Atividades/Atv3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char operador = ' ';
+
+        // Solicitar ao usuário o operador aritmético
+        Console.WriteLine("Digite um operador aritmético (+, -, *, /):");
+        while (operador != '+' && operador != '-' && operador != '*' && operador != '/')
+        {
+            operador = Convert.ToChar(Console.ReadLine());
+            if (operador != '+' && operador != '-' && operador != '*' && operador != '/')
+                Console.WriteLine("Operador aritmético inválido. Por favor, digite novamente:");
+        }
+
+        // Imprimir a tabuada com base no operador aritmético
+        Console.WriteLine($"Tabuada de {operador}:");
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                switch (operador)
+                {
+                    case '+':
+                        Console.Write($"{i} + {j} = {i + j}\t");
+                        break;
+                    case '-':
+                        Console.Write($"{i} - {j} = {i - j}\t");
+                        break;
+                    case '*':
+                        Console.Write($"{i} * {j} = {i * j}\t");
+                        break;
+                    case '/':
+                            Console.Write($"{i} / {j} = {(double)i / j}\t");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
